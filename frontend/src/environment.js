ffe886084995587b7432f1e3afd9ef9ccaff3aa9
Md Irfan <rn555666777@gmail.com>
2025-11-09 16:54:16 +0530

    Deploying Frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,8 +1,6 @@
 let IS_PROD = true;
-const server = IS_PROD ?
-    "https://apnacollegebackend.onrender.com" :
-
-    "http://localhost:8000"
-
+const server = IS_PROD
+  ? "https://zoom-clone-backend-30x4.onrender.com"
+  : "http://localhost:8000";
 
 export default server;
