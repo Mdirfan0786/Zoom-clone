294f963e08b87652ece9c8c111cfed108a0fa683
Md Irfan <rn555666777@gmail.com>
2026-02-15 02:28:32 +0530

    added .env file in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 import { createServer } from "node:http";
 import cors from "cors";
 import { connectToSocket } from "./controllers/socket.manager.js";
 import userRoute from "./routers/users.routes.js";
+
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
@@ -18,9 +21,7 @@ app.use(express.urlencoded({ limit: "40kb", extended: true }));
 app.use("/api/v1/users", userRoute);
 
 const start = async () => {
-  const connectiondb = await mongoose.connect(
-    "mongodb+srv://rn555666777_db_user:fZsx2javEPYxVWWs@apnazoom.wqfkobv.mongodb.net/apnaZoom?appName=apnaZoom",
-  );
+  const connectiondb = await mongoose.connect(process.env.MONGO_URL);
   console.log(`MONGO connected db host : ${connectiondb.connection.host}`);
 
   server.listen(app.get("port"), () => {
